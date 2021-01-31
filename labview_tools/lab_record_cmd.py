5a03e2c7dd48712f1ff9b643673aa4de8c91729b
James <kai.hsiang.ju@gmail.com>
2021-01-31 19:38:17 +0800

    delegate logic to server

---
+++ labview_tools/lab_record_cmd.py
@@ -0,0 +1,25 @@
+from cmd import Cmd
+from labview_tools import lab_start_record, lab_stop_record
+
+
+class MyPrompt(Cmd):
+    prompt = 'enter_prompt>> '
+    into = "Welcome! Type ? to list commands"
+
+    def __init__(self):
+        super(MyPrompt, self).__init__()
+
+    def do_exit(self, inp) -> bool:
+        print('Bye')
+        return True
+
+    def do_start_record(self, inp):
+        lab_start_record.start_recording()
+
+    def do_end_record(self, inp):
+        lab_stop_record.stop_recording()
+
+
+if __name__ == '__main__':
+    MyPrompt().cmdloop()
+    print('finished the cmd loop')
