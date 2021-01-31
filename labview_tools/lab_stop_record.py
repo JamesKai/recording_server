d5a090fcfd611bfff23b0e16c3946e8310e2370c
James <kai.hsiang.ju@gmail.com>
2021-01-31 16:46:43 +0800

    add checking to avoid invoking server twice

--- labview_tools/lab_stop_record.py
+++ labview_tools/lab_stop_record.py
@@ -2,5 +2,16 @@ import rpyc
 
 
 if __name__ == '__main__':
-    conn = rpyc.connect('localhost', port=18861).root.end_record()
-    print('stop recording now')
+    conn = None
+    try:
+        conn = rpyc.connect('localhost', port=18861)
+    except ConnectionRefusedError:
+        print('Server is not running ')
+    else:
+        # check if it is previously recording
+        if conn.root.status():
+            # start recording
+            conn.root.end_record()
+            print('was recording, stop recording now')
+    finally:
+        print('it is not recording')
