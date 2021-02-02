b7a2d00c86e5ccf7e1e80c3e6c57e71d553b8ce0
James <kai.hsiang.ju@gmail.com>
2021-02-02 17:51:45 +0800

    refactor

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from PySide2.QtWidgets import QApplication
-from record_gui import App
-import sys
-
+from labview_tools import lab_start_record
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+    lab_start_record.start_recording()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
