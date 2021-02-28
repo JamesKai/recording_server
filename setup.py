e65d89bb7dc149abf40f56c2b3d55d401b2d1b08
James <kai.hsiang.ju@gmail.com>
2021-02-28 19:36:16 +0800

    tune setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ScreenRecord',
-    version='0.1.1',
+    version='0.1.2',
     packages=['recording_tools', 'recording_tools.closable_server', 'recording_tools.recording_service',
               'telegram_tools', 'imaging_tools'],
     url='',
@@ -13,7 +13,7 @@ setup(
         "setuptools ~= 52.0.0",
         "rpyc ~= 5.0.1",
         "pyautogui ~= 0.9.52",
-        "opencv-python ~= 4.2.0.32",
+        "opencv-python ~= 4.2",
         "numpy",
         "python-telegram-bot ~= 13.2",
         "cnocr"]
